2de7e5779fb136ac203105636904dee710f17754
0NYX <125502645+0NYXcybersec@users.noreply.github.com>
2023-03-14 22:46:44 +0100

    Add files via upload

---
+++ P.W.H mk.1/P.W.H mk.1/P.W.H mk.1.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(8, INPUT); //input pin from soil sensor
+  pinMode(9, OUTPUT); //output pin for red LED
+  pinMode(10, OUTPUT); //output pin for green LED
+}
+
+void loop() {
+  int water = digitalRead(8);
+  digitalWrite(9, water == LOW ? HIGH : LOW);
+  digitalWrite(10, water == HIGH ? HIGH : LOW);
+  delay(250);
+}
